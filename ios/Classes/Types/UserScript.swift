c6d1c1f676c911d3e9d19fab98352587662099f7
João Carlos Martins Alves <joao.alves@meuvarejo360.com.br>
2022-09-20 15:02:45 -0300

    change ios version

--- ios/Classes/Types/UserScript.swift
+++ ios/Classes/Types/UserScript.swift
@@ -10,8 +10,6 @@ import WebKit
 
 public class UserScript : WKUserScript {
     var groupName: String?
-    @available(iOS 12.0, *)
-    lazy var contentWorld: WKContentWorld = WKContentWorld.page
     
     public override init(source: String, injectionTime: WKUserScriptInjectionTime, forMainFrameOnly: Bool) {
         super.init(source: source, injectionTime: injectionTime, forMainFrameOnly: forMainFrameOnly)
@@ -22,35 +20,12 @@ public class UserScript : WKUserScript {
         self.groupName = groupName
     }
     
-    @available(iOS 12.0, *)
-    public override init(source: String, injectionTime: WKUserScriptInjectionTime, forMainFrameOnly: Bool, in contentWorld: WKContentWorld) {
-        super.init(source: source, injectionTime: injectionTime, forMainFrameOnly: forMainFrameOnly, in: contentWorld)
-        self.contentWorld = contentWorld
-    }
-
-    @available(iOS 12.0, *)
-    public init(groupName: String?, source: String, injectionTime: WKUserScriptInjectionTime, forMainFrameOnly: Bool, in contentWorld: WKContentWorld) {
-        super.init(source: source, injectionTime: injectionTime, forMainFrameOnly: forMainFrameOnly, in: contentWorld)
-        self.groupName = groupName
-        self.contentWorld = contentWorld
-    }
-    
     public static func fromMap(map: [String:Any?]?, windowId: Int64?) -> UserScript? {
         guard let map = map else {
             return nil
         }
         
         let contentWorldMap = map["contentWorld"] as? [String:Any?]
-        if #available(iOS 12.0, *), let contentWorldMap = contentWorldMap {
-            let contentWorld = WKContentWorld.fromMap(map: contentWorldMap, windowId: windowId)!
-            return UserScript(
-                groupName: map["groupName"] as? String,
-                source: map["source"] as! String,
-                injectionTime: WKUserScriptInjectionTime.init(rawValue: map["injectionTime"] as! Int) ?? .atDocumentStart,
-                forMainFrameOnly: map["iosForMainFrameOnly"] as! Bool,
-                in: contentWorld
-            )
-        }
         return UserScript(
             groupName: map["groupName"] as? String,
             source: map["source"] as! String,
